72b31e4003ca0cf291e035e45373425af1ff9ab0
dongyuxi <orange2010road@gmail.com>
2014-12-17 23:04:40 +0800

    Add youmi.net ad code.

--- app/src/main/java/weather/today/orange/com/todayweather/TodayWeatherActivity.java
+++ app/src/main/java/weather/today/orange/com/todayweather/TodayWeatherActivity.java
@@ -3,17 +3,20 @@ package weather.today.orange.com.todayweather;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
-import org.json.JSONException;
-import org.json.JSONObject;
+import net.youmi.android.AdManager;
+import net.youmi.android.banner.AdSize;
+import net.youmi.android.banner.AdView;
+import net.youmi.android.banner.AdViewListener;
 
 import weather.today.orange.com.todayweather.weather.today.orange.com.todayweather.bean.WeatherInfo;
 import weather.today.orange.com.todayweather.weather.today.orange.com.todayweather.utils.BaiduWeatherUtils;
-import weather.today.orange.com.todayweather.weather.today.orange.com.todayweather.utils.HttpUtils;
-import weather.today.orange.com.todayweather.weather.today.orange.com.todayweather.utils.WeatherUtils;
 
 public class TodayWeatherActivity extends Activity {
 
@@ -21,6 +24,37 @@ public class TodayWeatherActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_today_weather);
+
+        // 初始化接口，应用启动的时候调用
+        // 参数：appId, appSecret, 调试模式
+        AdManager.getInstance(this).init("a274aa293ee86b3e", "f56dc6286bb90442", false);
+        // 实例化LayoutParams(重要)
+        FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.FILL_PARENT,
+                FrameLayout.LayoutParams.WRAP_CONTENT);
+        // 设置广告条的悬浮位置
+        layoutParams.gravity = Gravity.BOTTOM | Gravity.RIGHT; // 这里示例为右下角
+        // 实例化广告条
+        AdView adView = new AdView(this, AdSize.FIT_SCREEN);
+        // 调用Activity的addContentView函数
+        this.addContentView(adView, layoutParams);
+
+        // 监听广告条接口
+        adView.setAdListener(new AdViewListener() {
+            @Override
+            public void onSwitchedAd(AdView arg0) {
+                Log.i("YoumiAd", "广告条切换");
+            }
+
+            @Override
+            public void onReceivedAd(AdView arg0) {
+                Log.i("YoumiAd", "请求广告成功");
+            }
+
+            @Override
+            public void onFailedToReceivedAd(AdView arg0) {
+                Log.i("YoumiAd", "请求广告失败");
+            }
+        });
 
         if (android.os.Build.VERSION.SDK_INT > 9) {
             StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
